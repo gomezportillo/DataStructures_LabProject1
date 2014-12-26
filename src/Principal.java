577fc555b2ba812b830f32dd5a1a25bd8093eee8
Pedroma Gómez-Portillo <pedroga_tuenti@hotmail.com>
2014-12-26 17:57:30 +0100

    Create Principal.java

---
+++ src/Principal.java
@@ -0,0 +1,20 @@
+/**
+ * This is the main class of the proyect. From here, it is call the class Factory and made it work until it finish
+ * @author Pedro Manuel Gómez-Portillo López, David Ruiz Carrasco & Miriam Castillo Torroba
+ * @version 19.Oct.2014
+ */
+public class Principal {
+
+	public static void main(String[] args) {
+	
+		Factory factory = new Factory(144, 144, 12);
+
+		while(factory.working());
+
+		System.out.println("Total time: "+factory.getTime() +" time units");
+
+		factory.printStorageUnits();
+
+	}
+
+}
