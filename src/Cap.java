69868f08bc6d83230a646466d94dd6468357d2fe
Pedroma Gómez-Portillo <pedroga_tuenti@hotmail.com>
2014-12-26 18:00:11 +0100

    Create Cap.java

---
+++ src/Cap.java
@@ -0,0 +1,18 @@
+/**
+ * This class will implement the object cap
+ * @author Miriam Castillo Torroba
+ * @version 2.Oct.2014
+ */
+
+public class Cap {
+
+	String numberT;
+	
+	/**
+	 * This method will be the constructor of the class
+	 * @param t the identifier of the cap
+	 */
+	public Cap(String t){
+		this.numberT=t;
+	}
+}
