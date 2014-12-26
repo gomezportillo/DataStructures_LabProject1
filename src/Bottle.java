2ef0bf7d07a6759e264e69f5a074c94550c39712
Pedroma Gómez-Portillo <pedroga_tuenti@hotmail.com>
2014-12-26 17:57:07 +0100

    Create Bottle.java

---
+++ src/Bottle.java
@@ -0,0 +1,23 @@
+/**
+ * This class will implement the bottles
+ * @author Miriam Castillo Torroba
+ * @version 2.Oct.2014
+ */
+public class Bottle {
+
+	Cap cap;
+	String numberB;
+	
+	/**
+	 * This method is the constructor method of the class
+	 * @param b the identifier of the bottle
+	 */
+	public Bottle (String b){
+		this.cap=null;
+		this.numberB=b;	
+	}
+	
+	public void addCap(Cap c){
+		this.cap=c;
+	}
+}
